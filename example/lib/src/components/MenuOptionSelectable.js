0d1dbb943f333ee194c768708b2082e798244158
FreakyCoder <kurayogun@gmail.com>
2019-04-30 13:38:14 +0300

    Refactoring: MenuOptionSelectable & ReportModal

--- example/lib/src/components/MenuOptionSelectable.js
+++ example/lib/src/components/MenuOptionSelectable.js
@@ -1,52 +1,43 @@
-import React, { Component } from "react";
-import { Dimensions, Text, View, TouchableOpacity } from "react-native";
-import MyIcon from "react-native-custom-icon";
+import React from "react";
 import Divider from "./Divider";
+import MyIcon from "react-native-custom-icon";
+import styles, { itemTextStyle } from "./styles/MenuOptionSelectable.style";
+import { Dimensions, Text, View, TouchableOpacity } from "react-native";
 
 const { width } = Dimensions.get("window");
-
-const styles = {
-  container: {
-    flexDirection: "column"
-  },
-  iconTextContainer: {
-    left: 0,
-    flexDirection: "row"
-  },
-  itemTextStyle: {
-    left: 16,
-    fontSize: 15,
-    alignSelf: "center",
-    justifyContent: "center"
-    // fontFamily: "Metropolis-Medium"
-  },
-  dividerStyle: {
-    top: 8
-  }
+const MenuOptionSelectable = props => {
+  const {
+    item,
+    index,
+    length,
+    onPress,
+    iconConfig,
+    dividerWidth,
+    optionFontFamily
+  } = this.props;
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <View style={styles.iconTextContainer}>
+          <MyIcon
+            color={item.color}
+            config={iconConfig}
+            name={item.iconName}
+            size={item.iconSize}
+          />
+          <Text style={itemTextStyle(optionFontFamily, item.color)}>
+            {item.text}
+          </Text>
+        </View>
+        {index !== length - 1 && (
+          <Divider
+            horizontalStyle={styles.dividerStyle}
+            dividerWidth={dividerWidth || width * 0.62}
+          />
+        )}
+      </View>
+    </TouchableOpacity>
+  );
 };
 
-export default class MenuOptionSelectable extends Component {
-  render() {
-    const { item, index, length, onPress, iconConfig } = this.props;
-    return (
-      <TouchableOpacity onPress={onPress}>
-        <View style={styles.container}>
-          <View style={styles.iconTextContainer}>
-            <MyIcon
-              color={item.color}
-              config={iconConfig}
-              name={item.iconName}
-              size={item.iconSize}
-            />
-            <Text style={[styles.itemTextStyle, { color: item.color }]}>
-              {item.text}
-            </Text>
-          </View>
-          {index !== length - 1 && (
-            <Divider style={styles.dividerStyle} dividerWidth={width * 0.62} />
-          )}
-        </View>
-      </TouchableOpacity>
-    );
-  }
-}
+export default MenuOptionSelectable;
